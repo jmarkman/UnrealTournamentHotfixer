1acf926cc55c0fa7957e239578d636976e0e066f
Jonathan Markman <jmarkman446@gmail.com>
2021-05-02 17:10:06 -0400

    move ini edit logic from viewmodel's ApplyHotfix method to its own class

--- UnrealTournamentHotfixer/Services/ConfigEditor.cs
+++ UnrealTournamentHotfixer/Services/ConfigEditor.cs
@@ -1,22 +1,63 @@
 ﻿using IniParser;
+using IniParser.Model;
 using IniParser.Model.Configuration;
 using IniParser.Parser;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace UnrealTournamentHotfixer.Services
 {
     public class ConfigEditor
     {
-        private FileIniDataParser configParser;
+        /// <summary>
+        /// The filename and extension of the "User" config file
+        /// </summary>
         private readonly string UserConfigFile = "User.ini";
+
+        /// <summary>
+        /// The filename and extension of the "UT2004" config file
+        /// </summary>
         private readonly string UT2004ConfigFile = "UT2004.ini";
 
-        public ConfigEditor()
+        /// <summary>
+        /// The "System" directory in the game installation location
+        /// </summary>
+        private readonly string FilePath = string.Empty;
+
+        /// <summary>
+        /// The console command for changing packet send speed in UT2k4
+        /// </summary>
+        private readonly string NetspeedCommand = "netspeed";
+
+        /// <summary>
+        /// The "magic number" for adjusting netspeed
+        /// </summary>
+        private readonly string NetspeedValue = "10001";
+
+        /// <summary>
+        /// The most stable framerate that would provide a smooth experience
+        /// while not causing any weird physics issues or dips in framerate
+        /// </summary>
+        private readonly string StableMaxClientFrameRate = "112.0";
+
+        /// <summary>
+        /// The .ini file parser
+        /// </summary>
+        private FileIniDataParser configParser;
+
+        /// <summary>
+        /// The contents of the "User.ini" file
+        /// </summary>
+        private IniData userConfigData;
+
+        /// <summary>
+        /// The contents of the "UT2004.ini" file
+        /// </summary>
+        private IniData ut2004ConfigData;
+
+        public ConfigEditor(string filePath)
         {
+            FilePath = filePath;
+
             var parserConfig = new IniParserConfiguration
             {
                 AllowDuplicateKeys = true,
@@ -24,6 +65,84 @@ namespace UnrealTournamentHotfixer.Services
             };
 
             configParser = new FileIniDataParser(new IniDataParser(parserConfig));
+
+            userConfigData = configParser.ReadFile(Path.Combine(FilePath, UserConfigFile));
+            ut2004ConfigData = configParser.ReadFile(Path.Combine(FilePath, UT2004ConfigFile));
+        }
+
+        /// <summary>
+        /// Change the ConfiguredInternetSpeed setting in the User.ini file to unlock the
+        /// framerate. In practice this doesn't work for everyone and the 'netspeed toggle'
+        /// bind will be necessary, but we'll do it anyway.
+        /// </summary>
+        public void AdjustInternetSpeed()
+        {
+            var playerNetworkSettings = "Engine.Player";
+            var internetSpeedSetting = "ConfiguredInternetSpeed";
+
+            var internetSpeed = userConfigData[playerNetworkSettings][internetSpeedSetting];
+
+            if (internetSpeed != NetspeedValue)
+            {
+                userConfigData[playerNetworkSettings][internetSpeedSetting] = NetspeedValue;
+            }
+        }
+
+        /// <summary>
+        /// This will bind the "P" key (generally unused key) to the command "netspeed 10001",
+        /// which will adjust the number of packets being sent from the game client to the server
+        /// such that the frame rate won't be locked at 84 fps
+        /// </summary>
+        public void CreateNetspeedToggleBind()
+        {
+            var inputSettings = "Engine.Input";
+
+            userConfigData[inputSettings]["P"] = $"{NetspeedCommand} {NetspeedValue}";
+        }
+
+        /// <summary>
+        /// This will adjust the MaxClientFrameRate setting to the most stable maximum framerate I
+        /// found to work without odd frame dips
+        /// </summary>
+        public void AdjustMaxClientFrameRate()
+        {
+            var clientFramerateSection = "Engine.LevelInfo";
+            var clientFrameRateSetting = "MaxClientFrameRate";
+
+            ut2004ConfigData[clientFramerateSection][clientFrameRateSetting] = StableMaxClientFrameRate;
+        }
+
+        /// <summary>
+        /// For those who want to use the server browser, this applies a config fix 
+        /// that tells UT2k4 to ignore the Windows Firewall even though it can still
+        /// connect to servers using the "open" command from the firewall. Whatever.
+        /// </summary>
+        public void AddFirewallSectionIfNotPresent()
+        {
+            var firewallSection = "FireWall";
+            var ignoreSP2Setting = "IgnoreSP2";
+
+            if (ut2004ConfigData[firewallSection] == null)
+            {
+                ut2004ConfigData.Sections.AddSection(firewallSection);
+                ut2004ConfigData[firewallSection].AddKey(ignoreSP2Setting, "1");
+            }
+            else
+            {
+                if (ut2004ConfigData[firewallSection][ignoreSP2Setting] != "1")
+                {
+                    ut2004ConfigData[firewallSection][ignoreSP2Setting] = "1";
+                }
+            }
+        }
+
+        /// <summary>
+        /// Write the config changes to disk
+        /// </summary>
+        public void SaveChanges()
+        {
+            configParser.WriteFile(Path.Combine(FilePath, UserConfigFile), userConfigData);
+            configParser.WriteFile(Path.Combine(FilePath, UT2004ConfigFile), ut2004ConfigData);
         }
     }
 }
--- UnrealTournamentHotfixer/ViewModel/UTHotfixViewModel.cs
+++ UnrealTournamentHotfixer/ViewModel/UTHotfixViewModel.cs
@@ -1,11 +1,8 @@
-﻿using IniParser;
-using IniParser.Model.Configuration;
-using IniParser.Parser;
-using Ookii.Dialogs.Wpf;
+﻿using Ookii.Dialogs.Wpf;
 using System.ComponentModel;
-using System.IO;
 using System.Runtime.CompilerServices;
 using UnrealTournamentHotfixer.Command;
+using UnrealTournamentHotfixer.Services;
 
 namespace UnrealTournamentHotfixer.ViewModel
 {
@@ -55,71 +52,14 @@ namespace UnrealTournamentHotfixer.ViewModel
 
         public void ApplyHotfix()
         {
-            // TODO: Extract these to some kind of constants class
-            var userConfig = "User.ini";
-            var ut2k4Config = "UT2004.ini";
-            var playerNetworkSettings = "Engine.Player";
-            var inputSettings = "Engine.Input";
-            var netspeed = "netspeed";
-            var internetSpeedSetting = "ConfiguredInternetSpeed";
-            var firewallSection = "FireWall";
-            var ignoreSP2Setting = "IgnoreSP2";
-            var properFramerateInternetSpeed = "10001";
+            ConfigEditor configEditor = new ConfigEditor(FilePath);
 
-            /*
-             * Plan of action:
-             * 1. Read the User.ini
-             * 2. Find the "Engine.Player" section
-             * 3. Read the "ConfiguredInternetSpeed" setting
-             * 4. If the CIS setting isn't set to 10001, change it
-             * 5. Save the changes
-             * 6. Read the UT2004.ini
-             * 7. See if there's an existing section called "FireWall" before proceeding
-             * 8. Add a new section called "FireWall" if it isn't there
-             * 9. If the section exists, check that the key "IgnoreSP2" is set to "1"
-             * 10. If the "IgnoreSP2" key isn't set to 1, set it
-             * 11. If it doesn't exist, add the key-value pair "IgnoreSP2=1"
-             * 12. Save the changes
-             */
+            configEditor.AdjustInternetSpeed();
+            configEditor.CreateNetspeedToggleBind();
+            configEditor.AdjustMaxClientFrameRate();
+            configEditor.AddFirewallSectionIfNotPresent();
 
-            // The configs have duplicate keys with unique values, and
-            // IniParser defaults to "key = value" over UT's "key=value"
-            // convention. If the convention isn't followed, it'll break
-            // UT2k4 and cause a CD Key error or similar.
-            var parserConfig = new IniParserConfiguration
-            {
-                AllowDuplicateKeys = true,
-                AssigmentSpacer = string.Empty
-            };
-
-            var iniParser = new FileIniDataParser(new IniDataParser(parserConfig));
-
-            var userCfgData = iniParser.ReadFile(Path.Combine(FilePath, userConfig));
-
-            var internetSpeed = userCfgData[playerNetworkSettings][internetSpeedSetting];
-
-            if (internetSpeed != properFramerateInternetSpeed)
-            {
-                userCfgData[playerNetworkSettings][internetSpeedSetting] = properFramerateInternetSpeed;
-                iniParser.WriteFile(Path.Combine(FilePath, userConfig), userCfgData);
-            }
-
-            var ut2k4CfgData = iniParser.ReadFile(Path.Combine(FilePath, ut2k4Config));
-
-            if (ut2k4CfgData[firewallSection] == null)
-            {
-                ut2k4CfgData.Sections.AddSection(firewallSection);
-                ut2k4CfgData[firewallSection].AddKey(ignoreSP2Setting, "1");
-            }
-            else
-            {
-                if (ut2k4CfgData[firewallSection][ignoreSP2Setting] != "1")
-                {
-                    ut2k4CfgData[firewallSection][ignoreSP2Setting] = "1";
-                }
-            }
-
-            iniParser.WriteFile(Path.Combine(FilePath, ut2k4Config), ut2k4CfgData);
+            configEditor.SaveChanges();
 
             // This is Not Good™ - replace with a WindowService
             // demonstrated at https://stackoverflow.com/a/47353136
