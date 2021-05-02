310b0bbd9749cb1be9826c4431e75390b94b36d1
Jonathan Markman <jmarkman446@gmail.com>
2021-05-02 12:29:20 -0400

    Add project files.

---
+++ UnrealTournamentHotfixer/Command/DelegateCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace UnrealTournamentHotfixer.Command
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action execute;
+        private readonly Func<bool> canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public DelegateCommand(Action ex, Func<bool> canEx = null)
+        {
+            execute = ex ?? throw new ArgumentNullException(nameof(ex));
+            canExecute = canEx;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null ? true : canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            execute();
+        }
+    }
+}
---
+++ UnrealTournamentHotfixer/ViewModel/UTHotfixViewModel.cs
@@ -0,0 +1,62 @@
+﻿using Ookii.Dialogs.Wpf;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using UnrealTournamentHotfixer.Command;
+
+namespace UnrealTournamentHotfixer.ViewModel
+{
+    public class UTHotfixViewModel : INotifyPropertyChanged
+    {
+        private string filePath;
+
+        public string FilePath
+        {
+            get { return filePath; }
+            set 
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    filePath = value;
+                    OnPropertyChanged(nameof(FilePath));
+                    OnPropertyChanged(nameof(CanApplyHotfix));
+                    ApplyHotfixCommand.RaiseCanExecuteChanged();
+                }
+            }
+        }
+
+        public bool CanApplyHotfix => !string.IsNullOrEmpty(FilePath);
+        public DelegateCommand ApplyHotfixCommand { get; }
+        public DelegateCommand BrowseCommand { get; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public UTHotfixViewModel()
+        {
+            ApplyHotfixCommand = new DelegateCommand(ApplyHotfix, () => CanApplyHotfix);
+            BrowseCommand = new DelegateCommand(BrowseForGamePath);
+        }
+
+        public void BrowseForGamePath()
+        {
+            VistaFolderBrowserDialog fbd = new VistaFolderBrowserDialog
+            {
+                Description = "Locate the 'System' folder in the Unreal Tournament 2004 folder",
+                UseDescriptionForTitle = true
+            };
+
+            if ((bool)fbd.ShowDialog())
+            {
+                FilePath = fbd.SelectedPath;
+            }
+        }
+
+        public void ApplyHotfix()
+        {
+            
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
