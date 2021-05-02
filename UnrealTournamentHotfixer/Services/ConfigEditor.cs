08f7ad9e1bf3402c9cfbfa510655b786ac8aa1ce
Jonathan Markman <jmarkman446@gmail.com>
2021-05-02 16:11:20 -0400

    create class for handling the actual edit operations to remove clutter from vm

---
+++ UnrealTournamentHotfixer/Services/ConfigEditor.cs
@@ -0,0 +1,29 @@
+﻿using IniParser;
+using IniParser.Model.Configuration;
+using IniParser.Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnrealTournamentHotfixer.Services
+{
+    public class ConfigEditor
+    {
+        private FileIniDataParser configParser;
+        private readonly string UserConfigFile = "User.ini";
+        private readonly string UT2004ConfigFile = "UT2004.ini";
+
+        public ConfigEditor()
+        {
+            var parserConfig = new IniParserConfiguration
+            {
+                AllowDuplicateKeys = true,
+                AssigmentSpacer = string.Empty
+            };
+
+            configParser = new FileIniDataParser(new IniDataParser(parserConfig));
+        }
+    }
+}
